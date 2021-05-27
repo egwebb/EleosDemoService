4570929e9eb815524e614428db601c8ac970b92c
egwebb <xaubreon@gmail.com>
2021-05-27 13:31:25 -0400

    changed encoding schema

--- routes/api/authenticate.js
+++ routes/api/authenticate.js
@@ -10,7 +10,7 @@ router.get('/:token', (req, res) => {
     var jwtDecode = jwt_decode(req.params.token);
     User.findOne({ full_name: jwtDecode["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"] })
         .then((user) => {
-            user.api_token = jwt_encode({ "username": user.username, "full_name": user.full_name }, 'secret');
+            user.api_token = jwt_encode({ "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier": user.username, "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name": user.full_name }, 'secret');
             res.json(user);
         }).catch((err) => {
             console.log(err);
