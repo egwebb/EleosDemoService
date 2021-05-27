09318c54d8854eedf31c311f8285e3b5a61632e6
egwebb <xaubreon@gmail.com>
2021-05-27 12:19:59 -0400

    fixed authentication decode

--- routes/api/authenticate.js
+++ routes/api/authenticate.js
@@ -1,21 +1,18 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
+const { default: jwtDecode } = require('jwt-decode');
+const jwt_decode = require('jwt-decode');
+const jwt_encode = require('jwt-encode');
 const router = express.Router();
 
 const User = require('../../models/User');
 
 router.get('/:token', (req, res) => {
-    jwtDecode = jwt.decode(req.params.token);
-    //jwtName = jwtDecode.full_name;
-    User.findOne({ full_name: jwtDecode.name })
+    var jwtDecode = jwt_decode(req.params.token);
+    User.findOne({ full_name: jwtDecode["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"] })
         .then((user) => {
-            user.api_token = jwt.sign({ "username": user.username, "full_name": user.full_name }, 'secret');
+            user.api_token = jwt_encode({ "username": user.username, "full_name": user.full_name }, 'secret');
             res.json(user);
         }).catch(res.status(401));
 })
 
 module.exports = router;
-
-/*token => User.updateOne({ full_name: user.full_name}, { api_token: token}, user => res.json(user))*/
-
-/*user.api_token = jwt.sign({ "username": user.username, "full_name": user.full_name }, 'secret'), (user) => res.json(user)*/
